29a487cf1440fd609389429ca59f6bf537b5b655
Lini Abraham <liniabraham007@gmail.com>
2023-06-06 10:15:23 +1000

    added test to explore methods to find elements

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "@playwright/test";
+import { defineConfig } from '@playwright/test'
 
 /**
  * Read environment variables from file.
@@ -10,8 +10,8 @@ import { defineConfig } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: "./tests",
-  testMatch: "tests/explore-locators.ts",
+  testDir: './tests',
+  testMatch: 'tests/explore-locators.ts',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -21,28 +21,28 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: 'on-first-retry'
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: "chromium",
+      name: 'chromium',
       use: {
         // ...devices["Desktop Chrome"],
         viewport: null,
-        launchOptions: { args: ["--start-maximized"] },
-        trace: "on",
-        headless: false,
-      },
-    },
+        launchOptions: { args: ['--start-maximized'] },
+        trace: 'on',
+        headless: false
+      }
+    }
 
     // {
     //   name: "firefox",
@@ -73,7 +73,7 @@ export default defineConfig({
     //   name: 'Google Chrome',
     //   use: { ..devices['Desktop Chrome'], channel: 'chrome' },
     // },
-  ],
+  ]
 
   /* Run your local dev server before starting the tests */
   // webServer: {
@@ -81,4 +81,4 @@ export default defineConfig({
   //   url: 'http://127.0.0.1:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
-});
+})
--- tests/explore-locators.ts
+++ tests/explore-locators.ts
@@ -1,41 +1,41 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test'
 
-test("has title", async ({ page }) => {
-  await page.goto("https://bookcart.azurewebsites.net/");
-  await expect(page).toHaveTitle("BookCart");
-  await page.getByText("Login").click();
-  await page.getByLabel("Username").fill("ortoni");
-  await page.getByLabel("Password").fill("pass1234");
+test('has title', async ({ page }) => {
+  await page.goto('https://bookcart.azurewebsites.net/')
+  await expect(page).toHaveTitle('BookCart')
+  await page.getByText('Login').click()
+  await page.getByLabel('Username').fill('ortoni')
+  await page.getByLabel('Password').fill('pass1234')
   await Promise.all([
     page.waitForNavigation(),
-    page.locator("button.mat-primary").click(),
-  ]);
+    page.locator('button.mat-primary').click()
+  ])
   await page
-    .getByPlaceholder("Search books or authors")
-    .type("The Hate U Give");
-  console.log(`cwd is ${process.cwd()}`);
+    .getByPlaceholder('Search books or authors')
+    .type('The Hate U Give')
+  console.log(`cwd is ${process.cwd()}`)
   await page.screenshot({
     path: `${process.cwd()}/screenshots/screenshot.png`,
-    fullPage: true,
-  });
-  const options = page.getByRole("option");
-  await options.getByText("The Hate U Give").click();
+    fullPage: true
+  })
+  const options = page.getByRole('option')
+  await options.getByText('The Hate U Give').click()
   await page.screenshot({
     path: `${process.cwd()}/screenshots/select-book.png`,
-    fullPage: true,
-  });
-  await page.getByAltText("Book cover image").click();
+    fullPage: true
+  })
+  await page.getByAltText('Book cover image').click()
   await page.screenshot({
     path: `${process.cwd()}/screenshots/book-details.png`,
-    fullPage: true,
-  });
-  await page.getByText("Add to Cart").click();
+    fullPage: true
+  })
+  await page.getByText('Add to Cart').click()
   await page.screenshot({
     path: `${process.cwd()}/screenshots/added-to-cart.png`,
-    fullPage: true,
-  });
-  await page.pause();
-});
+    fullPage: true
+  })
+  await page.pause()
+})
 
 // test("get started link", async ({ page }) => {
 //   await page.goto("https://playwright.dev/");
